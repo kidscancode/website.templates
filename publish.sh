25ad31d69527689d64aa72cebaf1c90efd679d77
Chris Bradfield <cb@scribe.net>
2013-04-14 18:31:32 -0700

    js update

--- publish.sh
+++ publish.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 rsync -a --delete --exclude 'img' --exclude 'font' --exclude '.git' --exclude 'blog' --exclude 'CNAME' --exclude 'README.md' build/ ../kidscancode.github.com/
+rsync -a img/ ../kidscancode.github.com/img
